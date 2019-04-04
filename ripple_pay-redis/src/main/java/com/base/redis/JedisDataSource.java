3a81bc9e948a49f3d8dccd2cd0340fdf5a8a818b
chenka <chenka@ejar.com.cn>
2019-04-04 17:50:11 +0800

    删除redis模块

--- ripple_pay-redis/src/main/java/com/base/redis/JedisDataSource.java
+++
@@ -1,9 +0,0 @@
-package com.base.redis;
-
-import redis.clients.jedis.ShardedJedis;
-
-public interface JedisDataSource {
-     ShardedJedis getRedisClient();
-     void returnResource(ShardedJedis shardedJedis);
-     void returnResource(ShardedJedis shardedJedis, boolean broken);
-}
